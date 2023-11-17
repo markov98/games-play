06bd65809e0e68b92c404fc29b02132a8927a7e8
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-17 17:51:41 +0200

    added catalogue component and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home"
 import Login from './components/Login';
 import Register from './components/Register';
 import AddGame from './components/AddGame';
+import Catalogue from './components/Catalogue';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/add-game' element={<AddGame />} />
+          <Route path='/catalogue' element={<Catalogue />} />
         </Routes>
       </main>
 
