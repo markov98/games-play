c852615756eb748a19ca3fb89a7ea67312b8bea2
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-17 12:55:42 +0200

    added login and register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
 
   return (
     <div id="box">
       <Header />
-        <main id="main-content">
-          <Home />
-        </main>
+      <main id="main-content">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </main>
 
     </div>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <header>
-            <h1><Link className="home" to="#">GamesPlay</Link></h1>
+            <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="#">All games</Link>
 
@@ -12,8 +12,8 @@ export default function Header() {
                     <Link to="#">Logout</Link>
                 </div>
                 <div id="guest">
-                    <Link to="#">Login</Link>
-                    <Link to="#">Register</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
