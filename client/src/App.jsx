f54b66035b623d0d8881128249b1f574e16f071c
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-19 17:20:16 +0200

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from "./components/Header"
-import Home from "./components/Home"
-import Login from './components/Login';
-import Register from './components/Register';
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import AddGame from './components/AddGame/AddGame';
 import Catalogue from './components/Catalogue/Catalogue';
 
