70f64966f944fb5bfe277adc8bcc631031730a42
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-23 18:41:22 +0200

    updated useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,3 +1,28 @@
-export default function useForm() {
-    
+import { useState, useEffect } from "react";
+
+export default function useForm(submitHandler, initialValues) {
+    const [values, setValues] = useState(initialValues);
+
+    useEffect(() => {
+        setValues(initialValues);
+    }, [initialValues])
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values);
+    };
+
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
 }
