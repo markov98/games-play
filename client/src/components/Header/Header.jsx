d27a64a332297fe3f4aa76af237512a83946e6df
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-25 17:43:50 +0200

    updated nav so it is dynamic depending if use isAuthnticated

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
+import AuthContext from '../../contexts/authContext';
 
 export default function Header() {
+    const { isAuthencticated } = AuthContext;
+
     return (
         <header>
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/catalogue">All games</Link>
-
-                <div id="user">
-                    <Link to="/add-game">Create Game</Link>
-                    <Link to="#">Logout</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {isAuthencticated ?
+                    <div id="user">
+                        <Link to="/add-game">Create Game</Link>
+                        <Link to="#">Logout</Link>
+                    </div> :
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>}
             </nav>
         </header>
     )
