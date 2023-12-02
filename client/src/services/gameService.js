aa84736ec957705825d191a3b9e06fffb011948b
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-02 06:23:24 +0200

    updated gameService

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -8,6 +8,18 @@ export const getAllGames = async () => {
     return result;
 };
 
+export const getLatestGames = async () => {
+    const query = new URLSearchParams({
+        // sortBy: `_createdOn desc`,
+        offset: 0,
+        pageSize: 3,
+    });
+
+    const result = await request.get(`${baseUrl}?${query}`);
+
+    return result;
+}
+
 export const getOneGame = async (gameId) => {
     const result = await request.get(`${baseUrl}/${gameId}`, );
 
@@ -25,3 +37,5 @@ export const editGame = async (gameId, gameData) => {
 
     return result;
 };
+
+export const deleteGame = async (gameId) => request.remove(`${baseUrl}/${gameId}`);
