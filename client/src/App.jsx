b3cf575c9174796422e3c1093e8aee4e22d68697
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-02 06:19:58 +0200

    added AuthGuard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import GameDetails from './components/GameDetails/GameDetails';
 import Logout from "./components/Logout/Logout";
 import GameEdit from './components/EditGame/EditGame';
 import ErrorBoundary from './components/ErrorBoundary';
+import AuthGuard from './components/AuthGuard/AuthGuard';
 
 
 export default function App() {
@@ -25,11 +26,13 @@ export default function App() {
               <Route path='/' element={<Home />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
-              <Route path='/add-game' element={<AddGame />} />
-              <Route path='/catalogue' element={<Catalogue />} />
               <Route path="/games/:gameId" element={<GameDetails />} />
-              <Route path='/logout' element={<Logout />} />
-              <Route path='/games/:gameId/edit' element={<GameEdit />} />
+              <Route path='/catalogue' element={<Catalogue />} />
+              <Route element={<AuthGuard />}>
+                <Route path='/logout' element={<Logout />} />
+                <Route path='/games/:gameId/edit' element={<GameEdit />} />
+                <Route path='/add-game' element={<AddGame />} />
+              </Route>
             </Routes>
           </main>
 
